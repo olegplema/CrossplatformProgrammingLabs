1ecd4703bc2654fcb45aaedb0ec586154e9ca30c
Oleg Plema <olegplema01@gmail.com>
2024-10-05 21:00:56 +0300

    feat: implement input parsing and chord processing logic

--- Lab1/App/Program.cs
+++ Lab1/App/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using App;
+
+
+int frets;
+string[] tuning;
+string chord;
+try
+{
+    (frets, tuning, chord) = FileProcessor.ParseInputFile();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+    return;
+}
+
+tuning = ChordService.ChangeEnharmonicNotes(tuning);
+
+var chordNotes = ChordService.GetChordNotes(chord);
+
+var posibleWays = 0;
+
+try
+{
+    posibleWays = ChordService.CountWaysToPlayChord(tuning, frets, chordNotes);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+    return;
+}
+
+try
+{
+    FileProcessor.WriteOutputFile(posibleWays);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+}
