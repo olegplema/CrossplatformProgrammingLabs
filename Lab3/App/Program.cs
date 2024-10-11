e9dac611d8fad7dcaa12e405f3aa7703dea67d5f
Oleg Plema <olegplema01@gmail.com>
2024-10-11 19:42:48 +0300

    feat: calculations and output for channel connection optimization

--- Lab3/App/Program.cs
+++ Lab3/App/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using App;
+
+int n;
+List<(int, int)> channels;
+
+try
+{
+    (n, channels) = FileProcessor.ParseInputFile();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+    return;
+}
+
+int minStationsCount;
+List<HashSet<int>> optimalSolutions;
+
+try
+{
+    (minStationsCount, optimalSolutions) = ChanelService.FindOptimalStationSets(n, channels);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+    return;
+}
+
+try
+{
+    FileProcessor.WriteOutputFile(minStationsCount, optimalSolutions);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Виникла помилка: " + e.Message);
+}
